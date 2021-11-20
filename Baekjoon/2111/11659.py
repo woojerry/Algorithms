193fb7e4876e36f0d7c1e379fde4697381b0b05d
woojerry <woojerry@naver.com>
2021-11-20 12:29:15 +0900

    11/20

--- Baekjoon/2111/11659.py
+++ Baekjoon/2111/11659.py
@@ -1,11 +1,18 @@
 # 11/19
 # 11659 구간 합 구하기 4
 
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 
 numbers = list(map(int, input().split()))
+sum_list = [0]
+total = 0
+for i in range(len(numbers)):
+    total += numbers[i]
+    sum_list.append(total)
 
 for _ in range(M):
     start, end = map(int, input().split())
-
-print(numbers)
+    print(sum_list[end] - sum_list[start-1])
