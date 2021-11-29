00785a716c76291fa237017f835d4c7946e3e423
woojerry <woojerry@naver.com>
2021-11-29 15:44:39 +0900

    11/29

--- Others/2021 LINE Fintech/2.py
+++ Others/2021 LINE Fintech/2.py
@@ -1,26 +1,51 @@
-import itertools
+import math
 
 
 def solution(arr):
     answer = 0
-    sub_list = []
+    cand_list = []
+    pre, post = 0, 1
+    dir = False
+    start, end = 0, 0
 
-    changed = False
-    change_i = 0
-    after = 0
-    for i in range(1, len(arr) - 2):
+    while start < len(arr) - 2:
+        for end in range(start, len(arr) - 1):
+            if not dir:
+                if arr[end] > arr[end+1]:
+                    dir = True
+                    if end == (len(arr) - 2):
+                        cand_list.append(pre * post)
+                elif arr[end] < arr[end+1]:
+                    pre += 1
+                else:
+                    pre = 0
+                    start = end + 1
+                    break
+                if end == (len(arr) - 2):
+                    start = len(arr) - 2
+                    break
+            else:  # dir == True
+                if arr[end] > arr[end+1]:
+                    post += 1
+                    if end == (len(arr) - 2):
+                        cand_list.append(pre * post)
+                        start = len(arr) - 1
+                        break
+                else:
+                    cand_list.append(pre * post)
+                    pre, post = 0, 1
+                    dir = False
+                    start = end
+                    break
 
-        if not changed:
-            if arr[i] > arr[i+1]:
-                changed = True
-                change_i = i+1
-        else:
-            if arr[i] < arr[i+1]:
-                after = i - change_i
+    for j in cand_list:
+        answer += j
 
-                break
-    print(after * (change_i - 2))
-    return answer
+    answer = answer % (math.pow(10, 9) + 7)
+    return int(answer)
 
 
-print(solution([0, 1, 2, 5, 3, 7]))
+#print(solution([0, 1, 2, 5, 3, 7]))
+# print(solution([1, 2, 3, 2, 1]))
+print(solution([1, 2, 3, 2, 1, 4, 3, 2, 2, 1]))
+#print(solution([1, 2, 1, 2, 1]))
--- test.py
+++ test.py
@@ -1,38 +1,51 @@
-# for k in range(3, len(arr)):
-#     sub_list.append(list(itertools.combinations(arr, k)))
+import math
+# 선행으로 훑으면서 작아질 때 증가했던 거 답에 갯수 추가
+# 반대로 작아지다가 증가한 순간엔 증가했던 거 1로 초기화
 
-# for subs in sub_list:
-#     for sub in subs:
-#         print(sub)
-#         changed = False
-#         for i in range(len(sub)-1):
-#             if not changed:
-#                 if sub[i] > sub[i+1]:
-#                     changed = True
-#                     print(changed)
-#             else:
-#                 if sub[i] > sub[i+1]:
-#                     print(i)
-#                     if i == len(sub) - 1:
-#                         answer += 1
-#                 elif sub[i] <= sub[i+1]:
-#                     break
-# for k in range(3, len(arr)):
-#     sub_list.append(list(itertools.combinations(arr, k)))
 
-# for subs in sub_list:
-#     for sub in subs:
-#         print(sub)
-#         changed = False
-#         for i in range(len(sub)-1):
-#             if not changed:
-#                 if sub[i] > sub[i+1]:
-#                     changed = True
-#                     print(changed)
-#             else:
-#                 if sub[i] > sub[i+1]:
-#                     print(i)
-#                     if i == len(sub) - 1:
-#                         answer += 1
-#                 elif sub[i] <= sub[i+1]:
-#                     break
+def solution(arr):
+    answer = 0
+    cand_list = []
+    pre, post = 0, 1
+    dir = False
+    start, end = 0, 0
+
+    while start < len(arr) - 2:
+        for end in range(start, len(arr) - 1):
+            print(cand_list)
+            print(end, dir)
+            if not dir:
+                if arr[end] > arr[end+1]:
+                    pre = end
+                    dir = True
+                    if end == (len(arr) - 2):
+                        cand_list.append(pre * post)
+                else:
+                    pre += 1
+                if end == (len(arr) - 2):
+                    start = len(arr) - 2
+                    break
+
+            else:  # dir == True
+                if arr[end] > arr[end+1]:
+                    post += 1
+                    if end == (len(arr) - 2):
+                        cand_list.append(pre * post)
+                        start = len(arr) - 1
+                        break
+                else:
+                    cand_list.append(pre * post)
+                    pre, post = 1, 1
+                    dir = False
+                    start = end
+                    break
+    for j in cand_list:
+        answer += j
+
+    answer = answer % (math.pow(10, 9) + 7)
+    return int(answer)
+
+
+# print(solution([0, 1, 2, 5, 3, 7]))
+# print(solution([1, 2, 3, 2, 1]))
+print(solution([1, 2, 1, 2, 1]))
