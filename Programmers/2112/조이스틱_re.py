57e0f3119253a8589809f59d7a3812125e1eb5b2
woojerry <woojerry@naver.com>
2021-12-06 21:39:45 +0900

    12/06

--- Programmers/2112/조이스틱_re.py
+++ Programmers/2112/조이스틱_re.py
@@ -1,30 +1,56 @@
 def solution(name):
     answer = 0
-    alphabets = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9, "K": 10, "L": 11,
-                 "M": 12, "N": 13, "O": 12, "P": 11, "Q": 10, "R": 9, "S": 8, "T": 7, "U": 6, "V": 5, "W": 4, "X": 3, "Y": 2, "Z": 1}
+    up_down = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, "F": 5, "G": 6, "H": 7, "I": 8, "J": 9, "K": 10, "L": 11,
+               "M": 12, "N": 13, "O": 12, "P": 11, "Q": 10, "R": 9, "S": 8, "T": 7, "U": 6, "V": 5, "W": 4, "X": 3, "Y": 2, "Z": 1}
 
-    location = 1
-    up_down = []
-    for i in name:
-        up_down.append(alphabets[i])
+    visited = [1] * (len(name)-1)
+    visited[0] = 0
+    for i in range(len(name)):  # 가중치 더하기
+        answer += up_down[name[i]]
+        if name[i] == 'A':
+            visited[i] = 0
 
-    # for i in range(len(name)):
-    #     name[i]
-    for j in name:
-        if j == 'A':
-            count_A = 1
-            for k in range(j, len(name)-1):
-                if name[k] == 'A':
-                    count_A += 1
-                else:
-                    break
-            # 뒤에서부터 가는거리와 바로 가는 거리의 최소값..
+    cursor = 0
 
-    # 알파벳의 값 ..
-    # A는 무시 .. default
-    # continue 활용 ..? 기준 N
+    # for j in range(len(name)):  # 이동거리 더하기
+    while sum(visited) != 0:
+        left, right = cursor, cursor
+        left_cnt, right_cnt = 0, 0
+
+        while name[right] != 'A':
+            right += 1
+            right_cnt += 1
+            if right == len(name)-1:
+                right = 0
+        while name[left] != 'A':
+            left -= 1
+            left_cnt += 1
+            if left == -1:
+                left = len(name) - 1
+        if left_cnt >= right_cnt:
+            cursor = right
+            answer += right_cnt
+        else:
+            cursor = left
+            answer += left_cnt
+        visited[cursor] = 0
+        # if name[j] == 'A':
+        #     continue
+        # else:
+        #     right = abs(j - cursor)
+        #     left = abs(cursor + len(name) - j)
+        #     #print(right, left)
+        #     if right >= left:
+        #         answer += left
+        #     else:
+        #         answer += right
+        #     cursor = j
+        # print(name[j], answer)
 
     return answer
 
 
-print(solution('JAZ'))
+print(solution('ZAAAZZZZZZZ'))  # 15
+# print(solution('JAZ'))
+# print(solution("JEROEN"))
+# print(solution("JAN"))
