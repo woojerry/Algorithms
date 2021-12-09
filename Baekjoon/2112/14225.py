635fb9ad15e6eccc03937e4994fcc733fe6d5917
woojerry <woojerry@naver.com>
2021-12-09 23:21:09 +0900

    12/09

---
+++ Baekjoon/2112/14225.py
@@ -0,0 +1,20 @@
+# 12/9
+# 14225 부분수열의 합
+
+from itertools import combinations
+
+N = int(input())
+answer = 0
+num_set = set([])
+
+sequences = list(map(int, input().split()))
+
+for i in range(1, N+1):
+    combs = list(combinations(sequences, i))
+    for j in combs:
+        num_set.add(sum(j))
+
+for k in range(1, 2000000):
+    if k not in num_set:
+        print(k)
+        break
