99fac2ff0e42cf4b3a3496c257d07af33a1100c2
woojerry <woojerry@naver.com>
2021-12-13 21:44:13 +0900

    12/13

--- test.py
+++ test.py
@@ -1,12 +1,1 @@
-def palindrome(words):
-    is_palindrome = True
-    for i in range(len(words) // 2):
-        if words[i] != words[-1 - i]:      #
-            is_palindrome = False        # 회문이 아님
-            break
-    # print(words)
-
-    return is_palindrome
-
-
-print(palindrome('abc'))
+print(-1 % 2)
