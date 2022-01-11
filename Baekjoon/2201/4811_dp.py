b7a87bda5697fa2eb73d792d13e6347985c476be
woojerry <woojerry@naver.com>
2022-01-11 23:50:57 +0900

    01/11

--- Baekjoon/2201/4811_dp.py
+++ Baekjoon/2201/4811_dp.py
@@ -8,11 +8,11 @@ while True:
 
     dp = [[0] * (N+1) for _ in range(N+1)]
 
-    for i in range(N+1):  # 반개를 0번 먹었을 때
-        dp[0][i] = 1
+    for j in range(N+1):  # 알약을 1개만 먹었을 때
+        dp[0][j] = 1
 
-    for i in range(1, N+1):
-        for j in range(i, N+1):
+    for i in range(1, N+1):  # 반개
+        for j in range(i, N+1):  # 한개
             dp[i][j] = dp[i-1][j] + dp[i][j-1]
 
     print(dp[N][N])
